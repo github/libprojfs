72afff856b09c477efbf0159a06568c9782a57d6
Ashe Connor <ashe@kivikakk.ee>
2019-01-16 08:53:04 +1100

    licenses

--- docker/project.rb
+++ docker/project.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
+#
+# Linux Projected Filesystem
+# Copyright (C) 2019 GitHub, Inc.
+#
+# See the COPYING file distributed with this library for additional
+# information regarding copyright ownership.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library, in the file LICENSE.md; if not,
+# see <http://www.gnu.org/licenses/>.
 
 class Project
   def initialize(name, dockerfile:, image:, mounts: [], commands: {}, run_as_root: false, build_options: [], options: nil)
